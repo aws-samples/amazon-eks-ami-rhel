fe2514766480d8889967a250d824e11f7c3637ce
Jeremy McGee <jmcgeeal@amazon.com>
2024-11-13 16:25:24 -0700

    Added Path echo

--- templates/rhel/provisioners/install-worker.sh
+++ templates/rhel/provisioners/install-worker.sh
@@ -186,6 +186,7 @@ if [[ "$NERDCTL_URL" == *.rpm ]]; then
   # This is used if you need to use AL2 yum packages.
   # /usr/local/bin/nerdctl is not in the path for root.
   sudo sh -c "echo 'export PATH=\$PATH:/usr/local/bin' > /etc/profile.d/usr_local_bin.sh"
+  echo "Added /usr/local/bin to all users path"
 
 else
   # Download nerdctl tarball from S3 if an S3 URI is specified in the NERDCTL_URL environment variable
