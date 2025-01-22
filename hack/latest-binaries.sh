b54889832807376675ec13b4f4f08e5f29e536f4
Jeremy McGee <jmcgeeal@amazon.com>
2025-01-21 22:31:10 -0700

    Add S3 binary URL  and Bucket name to latest binary

--- hack/latest-binaries.sh
+++ hack/latest-binaries.sh
@@ -4,17 +4,54 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-if [ "$#" -ne 1 ]; then
-  echo "usage: $0 KUBERNETES_MINOR_VERSION"
+if [ "$#" -lt 1 ]; then
+  echo "usage: $0 KUBERNETES_MINOR_VERSION [BINARY_BUCKET_REGION] [BINARY_BUCKET_NAME]"
+  echo "  BINARY_BUCKET_REGION defaults to us-west-2"
+  echo "  BINARY_BUCKET_NAME defaults to amazon-eks"
   exit 1
 fi
 
 MINOR_VERSION="${1}"
+BINARY_BUCKET_REGION="${2:-us-west-2}"  # Default to us-west-2 if not provided
+BINARY_BUCKET_NAME="${3:-amazon-eks}"    # Default to amazon-eks if not provided
+
+# Set S3 domain based on region
+S3_DOMAIN="amazonaws.com"
+if [ "$BINARY_BUCKET_REGION" = "cn-north-1" ] || [ "$BINARY_BUCKET_REGION" = "cn-northwest-1" ]; then
+  S3_DOMAIN="amazonaws.com.cn"
+elif [ "$BINARY_BUCKET_REGION" = "us-iso-east-1" ] || [ "$BINARY_BUCKET_REGION" = "us-iso-west-1" ]; then
+  S3_DOMAIN="c2s.ic.gov"
+elif [ "$BINARY_BUCKET_REGION" = "us-isob-east-1" ]; then
+  S3_DOMAIN="sc2s.sgov.gov"
+elif [ "$BINARY_BUCKET_REGION" = "eu-isoe-west-1" ]; then
+  S3_DOMAIN="cloud.adc-e.uk"
+elif [ "$BINARY_BUCKET_REGION" = "us-isof-south-1" ]; then
+  S3_DOMAIN="csp.hci.ic.gov"
+fi
+
+# Construct the S3 endpoint URL
+S3_ENDPOINT="https://${BINARY_BUCKET_NAME}.s3.${BINARY_BUCKET_REGION}.${S3_DOMAIN}"
 
 # retrieve the available "VERSION/BUILD_DATE" prefixes (e.g. "1.28.1/2023-09-14")
 # from the binary object keys, sorted in descending semver order, and pick the first one
 # If s3api fails; use curl to pull the list.
-LATEST_BINARIES=$(aws s3api list-objects-v2 --region us-west-2 --no-sign-request --bucket amazon-eks --prefix "${MINOR_VERSION}" --query 'Contents[*].[Key]' --output text | grep -E '/[0-9]{4}-[0-9]{2}-[0-9]{2}/bin/linux' | cut -d'/' -f-2 | sort -Vru | head -n1 || curl -s  "https://amazon-eks.s3.amazonaws.com/?prefix=${MINOR_VERSION}" | xmllint --format  --nocdata - | grep -E  "<Key>${MINOR_VERSION}.*[0-9]{4}-[0-9]{2}-[0-9]{2}/bin/linux" | sed -E 's/.*<Key>([0-9]+\.[0-9]+\.[0-9]+\/[0-9]{4}-[0-9]{2}-[0-9]{2}).*/\1/' | sort -Vu | tail -n 1)
+LATEST_BINARIES=$(aws s3api list-objects-v2 \
+    --region "${BINARY_BUCKET_REGION}" \
+    --no-sign-request \
+    --bucket "${BINARY_BUCKET_NAME}" \
+    --prefix "${MINOR_VERSION}" \
+    --query 'Contents[*].[Key]' \
+    --output text | \
+    grep -E '/[0-9]{4}-[0-9]{2}-[0-9]{2}/bin/linux' | \
+    cut -d'/' -f-2 | \
+    sort -Vru | \
+    head -n1 || \
+    curl -s "${S3_ENDPOINT}/?prefix=${MINOR_VERSION}" | \
+    xmllint --format --nocdata - | \
+    grep -E "<Key>${MINOR_VERSION}.*[0-9]{4}-[0-9]{2}-[0-9]{2}/bin/linux" | \
+    sed -E 's/.*<Key>([0-9]+\.[0-9]+\.[0-9]+\/[0-9]{4}-[0-9]{2}-[0-9]{2}).*/\1/' | \
+    sort -Vu | \
+    tail -n 1)
 
 if [ "${LATEST_BINARIES}" == "None" ]; then
   echo >&2 "No binaries available for minor version: ${MINOR_VERSION}"
